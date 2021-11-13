30a64e1cd12c5ae502ff7fdaa83f1515934e0358
Jamie Mason <jamie@foldleft.io>
2021-11-13 17:29:16 +0000

    fix(npm): update dependencies

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,12 @@ module.exports = {
       branches: 95,
       functions: 95,
       lines: 95,
-      statements: 95
-    }
+      statements: 95,
+    },
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  testMatch: ['<rootDir>/src/**/*.spec.(ts|tsx|js)'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
